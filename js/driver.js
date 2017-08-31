a11a6782d9713cd37d15450850bc5449989f716c
chjzc <zhuochenjin@163.com>
2017-08-31 21:03:05 +0800

    'update'

--- js/driver.js
+++ js/driver.js
@@ -4,7 +4,7 @@
 */
 var correlate = vis.correlate(),
 	runner = taskrunner();
-var data_num=2;
+var data_num=1;
 var glyph_num=3;
 
 var start_time = new Date(2016, 1, 3, 0, 59, 0).getTime() / 1000;
